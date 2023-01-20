5d76a5c3f68fc42fd2d82b3e31a648fd7b538989
kento <kento@toriwatari.bird>
2023-01-20 20:34:59 +0900

    ✨ Update

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,8 @@
 import express from "express"
 import { initTRPC } from "@trpc/server"
 import * as trpcExpress from "@trpc/server/adapters/express"
-import cors from 'cors';
+import cors from 'cors'
+import { z } from "zod"
 
 const app = express()
 app.use(cors())
@@ -11,7 +12,16 @@ const t = initTRPC.create()
 const appRouter = t.router({
   hello: t.procedure.query(() => {
     return `Hello World`
-  })
+  }),
+  helloName: t.procedure
+    // input 受け取れるデータの定義
+    .input(z.object({ name: z.string(), age: z.number() }))
+    .query(({ input }) => {
+      return {
+        name: `名前は${input.name}です。`,
+        age: `年齢は${input.age}歳です。` 
+      }
+    })
 })
 
 app.use(
