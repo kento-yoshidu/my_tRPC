e40a5fecdb1f89aafe7fc6593e3eb4eeddd397c3
kento <kento@toriwatari.bird>
2023-01-21 19:37:15 +0900

    ✨ Update

--- server/main.ts
+++ server/main.ts
@@ -1,18 +1,26 @@
-import express from "express"
-import { initTRPC } from "@trpc/server"
-import * as trpcExpress from "@trpc/server/adapters/express"
-import cors from 'cors'
-import { z } from "zod"
+import express from 'express';
+import { inferAsyncReturnType, initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import cors from 'cors';
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-const t = initTRPC.create()
+const prisma = new PrismaClient();
+const createContext = (opts: trpcExpress.CreateExpressContextOptions) => {
+  return { prisma };
+};
+
+type Context = inferAsyncReturnType<typeof createContext>
+const t = initTRPC.context().create();
 
 const appRouter = t.router({
   hello: t.procedure.query(() => {
     return `Hello World`
   }),
+
   helloName: t.procedure
     // input 受け取れるデータの定義
     .input(z.object({ name: z.string(), age: z.number() }))
@@ -21,7 +29,13 @@ const appRouter = t.router({
         name: `名前は${input.name}です。`,
         age: `年齢は${input.age}歳です。` 
       }
-    })
+    }),
+
+  todos: t.procedure.query(async ({ ctx }) => {
+    console.log(ctx) 
+    // const data = await ctx.prisma.todo.findMany()
+    return { data: "hoge" }
+  }),
 })
 
 app.use(
